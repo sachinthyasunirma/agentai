ac9580f3cb6a4a91f9bf36563f8413ed23a57c24
sachinthya <ssrathnavibushana@gmail.com>
2025-06-08 23:45:14 +0530

    agent filters

---
+++ src/modules/agents/hooks/use-agents-filters.ts
@@ -0,0 +1,12 @@
+import { useQueryStates, parseAsInteger, parseAsString } from "nuqs";
+
+import { DEFAULT_PAGE } from "@/constants";
+
+export const useAgentsFilters = () => {
+  return useQueryStates({
+    search: parseAsString.withDefault("").withOptions({ clearOnDefault: true }),
+    page: parseAsInteger
+      .withDefault(DEFAULT_PAGE)
+      .withOptions({ clearOnDefault: true }),
+  });
+};
